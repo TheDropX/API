b45473f65bbe8390acede7a13db551ecc702452d
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-06 02:22:32 +0300

    Update presencesRoutes.ts

--- lib/routes/presencesRoutes.ts
+++ lib/routes/presencesRoutes.ts
@@ -10,7 +10,7 @@ export class Routes {
         app.route('/test')
         .get((req: Request, res: Response) => {            
             res.status(200).send({
-                message: 'test'
+                message: 'testing...'
             })
         })
         
