85b7397c784b4f5249cf426afbbbf4c123373e2b
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-09-07 02:34:28 +0300

    testing...

--- lib/routes/presencesRoutes.ts
+++ lib/routes/presencesRoutes.ts
@@ -1,13 +1,17 @@
 import {Request, Response, NextFunction} from "express";
 import { PresencesController } from "../controllers/presencesController";
 
+var childProcess = require('child_process');
+var githubUsername = 'TheDropX'
+
+
 export class Routes { 
     
     public presencesController: PresencesController = new PresencesController() 
     
     public routes(app): void {   
 
-        app.route('/')
+        app.route('/webhooks/github')
         .get((req: Request, res: Response) => {            
             res.status(200).send({
                 message: 'Test.'
@@ -28,7 +32,27 @@ export class Routes {
             console.log(`Request from: ${req.originalUrl}`);
             console.log(`Request type: ${req.method}`);            
             next();                  
-        }, this.presencesController.getPresenceByUserID)        
+        }, this.presencesController.getPresenceByUserID)      
+        
+        app.post("/webhooks/github", function (req, res) {
+            var sender = req.body.sender;
+            var branch = req.body.ref;
+        
+            if(branch.indexOf('master') > -1 && sender.login === githubUsername){
+                deploy(res);
+            }
+        })
+        
+        function deploy(res){
+            childProcess.exec('cd /home/alex/Webhook && ./DeployNow.sh', function(err, stdout, stderr){
+                if (err) {
+                 console.error(err);
+                 return res.send(500);
+                }
+                res.send(200);
+              });
+        }
+        
 
     }
 }
