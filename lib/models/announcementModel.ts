22300e6e622ec5149338aa807f99f8bb3f5cb0b4
TheDrop <35700745+TheDropX@users.noreply.github.com>
2019-10-16 23:44:38 +0300

    :sparkles: Updated.

--- lib/models/announcementModel.ts
+++ lib/models/announcementModel.ts
@@ -6,6 +6,7 @@ export const announcementSchema = new Schema({
 
     title: { type: String, required: true },
     message: { type: String, required: true },
-    version: { type: String, required: true }
+    version: { type: String, required: true },
+    link: { type: String, required: true }
 
 });
